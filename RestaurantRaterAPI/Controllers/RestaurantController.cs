9cd2d8e567305638e40bece68bfad974b73ebf5e
Jon Kline <jonphilk@gmail.com>
2020-08-25 15:11:27 -0400

    Build out GET methods

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,60 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        RestaurantDbContext _context = new RestaurantDbContext();
+
+        //-- Create (POST)
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok("You created a restaurant and it was saved!");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        //-- Read (GET)
+        // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+
+            return NotFound();
+        }
+        // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok();
+        }
+        //-- Update (PUT)
+
+        //-- Delete (DELETE)
+    }
+}
